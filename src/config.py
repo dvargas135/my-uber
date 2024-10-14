89b267ad1771bd4eda00cdcb7f76858d8318b3e9
Daniel Vargas <dvargasp@javeriana.edu.co>
2024-10-13 19:48:43 -0500

    Add valid speeds and max matrix size

---
+++ src/config.py
@@ -0,0 +1,12 @@
+# ZeroMQ Configuration
+DISPATCHER_IP = "localhost"
+PUB_PORT = 5555
+SUB_PORT = 5556
+REP_PORT = 5557
+
+# Default speed values
+VALID_SPEEDS = [1, 2, 4]
+
+# Grid configuration
+MAX_N = 1000
+MAX_M = 1000

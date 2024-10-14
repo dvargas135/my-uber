946d4a98ebf288457453f9b0000ab27c9520c44e
Daniel Vargas <dvargasp@javeriana.edu.co>
2024-10-13 19:51:04 -0500

    Temporary user attributes

---
+++ src/models/user_model.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, user_id, pos_x, pos_y, timeout):
+        self.user_id = user_id
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.timeout = timeout

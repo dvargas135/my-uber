e56c6548332c7ac9950b96fea2c7b9113aebd3ae
Daniel Vargas <dvargasp@javeriana.edu.co>
2024-10-31 21:51:25 -0500

    Added pull and removed unnecessary functions

--- src/utils/zmq_utils.py
+++ src/utils/zmq_utils.py
@@ -1,37 +1,29 @@
 import zmq
 
 class ZMQUtils:
-    def __init__(self, dispatcher_ip, pub_port, sub_port, rep_port):
+    def __init__(self, dispatcher_ip, pub_port, sub_port, rep_port, pull_port):
         self.context = zmq.Context()
         self.dispatcher_ip = dispatcher_ip
         self.pub_port = pub_port
         self.sub_port = sub_port
         self.rep_port = rep_port
+        self.pull_port = pull_port
         self.publisher = None
         self.subscriber = None
         self.requester = None
         self.responder = None
+        self.puller = None
+        self.pusher = None
 
     def bind_pub_socket(self):
         self.publisher = self.context.socket(zmq.PUB)
         self.publisher.bind(f"tcp://*:{self.pub_port}")
         return self.publisher
-
-    def bind_sub_socket(self, topic=""):
-        self.subscriber = self.context.socket(zmq.SUB)
-        self.subscriber.bind(f"tcp://*:{self.sub_port}")
-        self.subscriber.setsockopt_string(zmq.SUBSCRIBE, topic)
-        return self.subscriber
     
     def bind_rep_socket(self):
         self.responder = self.context.socket(zmq.REP)
         self.responder.bind(f"tcp://*:{self.rep_port}")
         return self.responder
-    
-    def connect_pub(self):
-        self.publisher = self.context.socket(zmq.PUB)
-        self.publisher.connect(f"tcp://{self.dispatcher_ip}:{self.pub_port}")
-        return self.publisher
 
     def connect_sub(self, topic=""):
         self.subscriber = self.context.socket(zmq.SUB)
@@ -43,6 +35,16 @@ class ZMQUtils:
         self.requester = self.context.socket(zmq.REQ)
         self.requester.connect(f"tcp://{self.dispatcher_ip}:{self.rep_port}")
         return self.requester
+    
+    def bind_pull_socket(self):
+        self.puller = self.context.socket(zmq.PULL)
+        self.puller.bind(f"tcp://*:{self.pull_port}")
+        return self.puller
+
+    def connect_push(self):
+        self.pusher = self.context.socket(zmq.PUSH)
+        self.pusher.connect(f"tcp://{self.dispatcher_ip}:{self.pull_port}")
+        return self.pusher
     
     def disconnect_pub(self):
         if self.publisher:
@@ -69,4 +71,8 @@ class ZMQUtils:
             self.requester.close()
         if self.responder:
             self.responder.close()
+        if self.puller:
+            self.puller.close()
+        if self.pusher:
+            self.pusher.close()
         self.context.term()
